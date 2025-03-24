97fb9e62e40ddd96b3b75eb3f10801e3a7fac6e2
keshew <artyom.korotckov2017@yandex.ru>
2025-03-24 17:59:18 +0500

    Initial Commit

---
+++ Ballon/BallonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallonApp.swift
+//  Ballon
+//
+//  Created by Артём Коротков on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BallonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
